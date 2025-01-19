cf68db80fd65cb58211bf5fdcba59c33cba073ee
shinminchul <su99king@gmail.com>
2025-01-19 15:03:45 +0900

    chul01

--- World.cpp
+++ World.cpp
@@ -1,5 +1,6 @@
 #include "World.h"
-
+#include <iostream>
+using namespace std;
 
 World::World(int n)
 	: number(n) {}
@@ -17,12 +18,42 @@ int main() {
 	HealingPotion hp(20, 2);
 	ManaPotion mp(20, 2);
 
-	cout << "Basic Sword - Price: " << basicSword.price << ", Defense Power: " << basicSword.defensePower << ", Power: " << basicSword.power << endl;
-	cout << "High Quality Sword - Price: " << highQualitySword.price << ", Defense Power: " << highQualitySword.defensePower << ", Power: " << highQualitySword.power << endl;
-	cout << "Warrior Sword - Price: " << warriorSword.price << ", Defense Power: " << warriorSword.defensePower << ", Power: " << warriorSword.power << endl;
-	cout << "Magician Stick - Price: " << magicianStick.price << ", Defense Power: " << magicianStick.defensePower << ", Power: " << magicianStick.power << endl;
+    // 검 확인
+    cout << "Basic Sword - Price: " << basicSword.price
+        << ", Defense Power: " << basicSword.defensePower
+        << ", Power: " << basicSword.power << endl;
 
-	cout << "Low Armor - Price: " << lowArmor.price << ", Defense Power: " << lowArmor.defensePower << ", Power: " << lowArmor.power << endl;
-	cout << "Middle Armor - Price: " << middleArmor.price << ", Defense Power: " << middleArmor.defensePower << ", Power: " << middleArmor.power << endl;
-	cout << "High Armor - Price: " << highArmor.price << ", Defense Power: " << highArmor.defensePower << ", Power: " << highArmor.power << endl;
+    cout << "High Quality Sword - Price: " << highQualitySword.price
+        << ", Defense Power: " << highQualitySword.defensePower
+        << ", Power: " << highQualitySword.power << endl;
+
+    cout << "Warrior Sword - Price: " << warriorSword.price
+        << ", Defense Power: " << warriorSword.defensePower
+        << ", Power: " << warriorSword.power << endl;
+
+    cout << "Magician Stick - Price: " << magicianStick.price
+        << ", Defense Power: " << magicianStick.defensePower
+        << ", Power: " << magicianStick.power << endl;
+
+    // 방어구 확인
+    cout << "Low Armor - Price: " << lowArmor.price
+        << ", Defense Power: " << lowArmor.defensePower
+        << ", Power: " << lowArmor.power << endl;
+
+    cout << "Middle Armor - Price: " << middleArmor.price
+        << ", Defense Power: " << middleArmor.defensePower
+        << ", Power: " << middleArmor.power << endl;
+
+    cout << "High Armor - Price: " << highArmor.price
+        << ", Defense Power: " << highArmor.defensePower
+        << ", Power: " << highArmor.power << endl;
+
+    // 포션 확인
+    cout << "Healing Potion - Price: " << hp.price
+        << ", Drop Percent: " << hp.dropPercent << endl;
+
+    cout << "Mana Potion - Price: " << mp.price
+        << ", Drop Percent: " << mp.dropPercent << endl;
+
+	return 0;
 }
